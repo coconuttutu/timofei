499f0bc2e961457599c2a198cee91e5353fd5b79
coconuttutu <158302736+coconuttutu@users.noreply.github.com>
2024-12-18 13:25:08 +0300

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,73 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{   
+    setlocale(LC_CTYPE, "RU");
+    int a, b, c, n;
+    cout << "Введите номер задания: ";
+    cin >> n;
+    switch (n) 
+    {
+    case 1:
+    {
+        cout << "Введите первое число: ";
+        cin >> a;
+        cout << endl;
+
+        cout << "Введите второе  число: ";
+        cin >> b;
+
+        int max = a > b ? a : b;
+        cout << "Максимальное число: " << max;
+    }
+    case 2:
+    {
+        a = 0;
+        cout << "Введите  число: ";
+        cin >> a;
+        cout << endl;
+        (a % 2 == 0) ? cout << "Число чётное" : cout << "Число нечётное";
+ 
+    }
+
+    case 3:
+    {
+        a = 0;
+        cout << "Введите  число: ";
+        cin >> a;
+        cout << endl;
+        (a > 0) ? cout << "Число положительное" : ( a < 0 ) ?  cout << "Число отрицательное " : cout << "Число равно нулю  ";
+    }
+
+    case 4: 
+    {
+        a = 0, b = 0, c = 0;
+        cout << "Введите первое число: ";
+        cin >> a;
+        cout << endl;
+
+        cout << "Введите второе  число: ";
+        cin >> b;
+        cout << endl;
+
+        cout << "Введите третье  число: ";
+        cin >> c;
+        cout << endl;
+
+
+
+        int max = (a > b) ?  (a > c) ? a : (b > c) ? c : b : b;
+        cout << "Максимальное число: " << max;
+    }
+
+
+    }
+
+  
+}
+
+
